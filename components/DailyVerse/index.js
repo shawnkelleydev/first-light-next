d5a3c5bd635bc4c0c4e66c0f6848b8e74bc661c1
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-22 09:39:13 -0600

    Add get another verse && link animation

--- components/DailyVerse/index.js
+++ components/DailyVerse/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-
 import { Interweave } from 'interweave'
+
+import Link from 'components/Link'
 
 import styles from './styles.module.css'
 
@@ -11,7 +12,12 @@ export default function DailyVerse({ passageData }) {
   useEffect(() => {
     if (passageData) {
       let html = passageData.passages[0]
-      html = html.replace('<b', '<sup').replace('</b>', '</sup>')
+
+      html = html
+        .replaceAll('<b', '<sup')
+        .replaceAll('</b>', '</sup>')
+        .replaceAll('\n', '')
+
       setPassage(html)
       setCitation(passageData.canonical)
     }
@@ -19,6 +25,9 @@ export default function DailyVerse({ passageData }) {
 
   return (
     <div className={styles['daily-verse']}>
+      <div>
+        <Link url='/'>get another verse</Link>
+      </div>
       <blockquote>
         <Interweave content={passage} />
       </blockquote>

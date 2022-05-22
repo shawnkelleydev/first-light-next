ebb51ce8c5c674eb273994f82b08edd37943c5e6
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-22 09:17:27 -0600

    Add home query parameter for DailyVerse

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 import { getEarthPic } from 'services/nasa'
-import { getRandomPassageData } from 'services/esv'
+import { getPassageData } from 'services/esv'
+
+import { getRandomVerse } from 'utils/esv'
 
 import DailyVerse from 'components/DailyVerse'
 import EarthPic from 'components/EarthPic'
@@ -10,26 +13,31 @@ import PageWrapper from 'components/PageWrapper'
 
 import styles from './styles.module.css'
 
-export default function Home() {
-  const [earthPic, setEarthPic] = useState(null)
+export default function Home({ earthPic }) {
   const [randomPassageData, setRandomPassageData] = useState(null)
-
   const [loading, setLoading] = useState(true)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const verse = getRandomVerse()
+    if (!router.query.q) router.push(`/?q=${verse}`)
+  }, [router])
 
   useEffect(() => {
     setLoading(true)
     ;(async () => {
-      const pic = await getEarthPic()
-      const data = await getRandomPassageData()
+      const query = router.query.q
+      if (!query) return
 
-      setEarthPic(pic)
+      const data = await getPassageData(query)
+
       setRandomPassageData(data)
-
       setLoading(false)
     })()
-  }, [])
+  }, [router])
 
-  if (loading)
+  if (loading || !earthPic)
     return (
       <PageWrapper>
         <Loader />
@@ -46,4 +54,13 @@ export default function Home() {
       </div>
     </PageWrapper>
   )
+}
+
+export const getStaticProps = async () => {
+  const earthPic = await getEarthPic()
+  return {
+    props: {
+      earthPic,
+    },
+  }
 }
